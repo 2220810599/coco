ccc9d2f13362ca8dc852a3a7a426d73fe1ec0312
2220810599 <2220810599@qq.com>
2019-09-04 08:17:51 +0800

    1.01

--- wx-auth-util/src/main/java/com/coco/CocoHttpSent.java
+++ wx-auth-util/src/main/java/com/coco/CocoHttpSent.java
@@ -3,21 +3,11 @@ package com.coco;
 
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLEncoder;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
- 
 
  
 public class CocoHttpSent {
